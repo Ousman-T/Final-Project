031c68dca2262be15a536ac63f9cd56e2c6496d8
Ousman Touray <otouray14@gmail.com>
2023-04-27 14:03:53 -0400

    restructuring front end

--- src/pages/NewOrderPage.js
+++ src/pages/NewOrderPage.js
@@ -1,7 +1,22 @@
 
 
 function NewOrderPage() {
-    return <h1>NewOrderPage</h1>
+    return(
+    <div>
+        <h1>Create a new ToDo</h1>
+        <form action="/orders/new" method="POST">
+            ToDo Name:<input type="text" name="ToDo Name"/>
+            <br />
+            ToDo Details:<input type="text" name="ToDo Details"/>
+            <br />
+            Done?<input type="checkbox" name="Done?"/>
+            <br />
+            <input type="submit" value='Create To Do'/>
+
+        </form>
+    </div>
+        )
+    
 }
 
 
