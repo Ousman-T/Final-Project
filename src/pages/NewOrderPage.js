a679937534f8d6b05174d4fa545b44993adbfb11
Ousman Touray <otouray14@gmail.com>
2023-04-28 13:58:10 -0400

    trying to fix state and todo rendering

--- src/pages/NewOrderPage.js
+++ src/pages/NewOrderPage.js
@@ -1,17 +1,30 @@
-
+import { useState } from "react";
 
 function NewOrderPage() {
+    const [newTodo, setNewTodo] = useState({
+      todo:  "",
+      todoDetails: "",
+      done: false
+    })
+
+    const handleChange = (evt) => {
+        setNewTodo({...newTodo, [evt.target.name]: evt.target.value, error: ''})
+    }
+
+    const handleSubmit = async (e) => {
+        handleChange()
+    };
     return(
     <div>
         <h1>Create a new ToDo</h1>
-        <form action="/orders/new" method="POST">
+        <form  autoComplete="off" action="/orders/new" method="POST" onSubmit={handleSubmit}>
             ToDo Name:<input type="text" name="ToDo Name"/>
             <br />
             ToDo Details:<input type="text" name="ToDo Details"/>
             <br />
             Done?<input type="checkbox" name="Done?"/>
             <br />
-            <input type="submit" value='Create To Do'/>
+            <button type="submit" onSubmit={handleSubmit}>Create To Do</button>
 
         </form>
     </div>
--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -20,6 +20,7 @@ export function login(credentials) {
   return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
 }
 
+
 //* Check Token
 export function checkToken() {
     return sendRequest(`${BASE_URL}/check-token`)
